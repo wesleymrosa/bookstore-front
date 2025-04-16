999ff5d66148c53b17aa460c9aaec817995ccfdb
Wesley Rosa <wesleymrosa@gmail.com>
2025-04-16 16:06:59 -0300

    Adicionado botão 'Cancelar' em atualizar categoria para redirecionar para categorias; Implementado tratamento de erro para evitar atualização com campos vazios.

--- src/app/components/views/categoria/categoria-update/categoria-update.component.ts
+++ src/app/components/views/categoria/categoria-update/categoria-update.component.ts
@@ -48,7 +48,8 @@ export class CategoriaUpdateComponent implements OnInit {
       this.router.navigate(['categorias']);
       this.service.mensagem("Categoria atualizada com sucesso");
     }, err => {
-      console.log(err);
+      this.service.mensagem("Validar se todos os campos estão preenchidos corretamente!");
+
     });
   }
 
